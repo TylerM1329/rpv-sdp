a455a738e5b20d1be6ae40eef251d68f658a7969
MrGayman <88008841+TylerM1329@users.noreply.github.com>
2024-11-17 17:26:38 -0800

    GPS updated but course still not consistent

--- subsystems/telemetry_subsystem/telemetry_subsystem.ino
+++ subsystems/telemetry_subsystem/telemetry_subsystem.ino
@@ -9,12 +9,12 @@ static const uint32_t GPSBaud = 9600;
 SoftwareSerial gpsSerial(RXPin, TXPin);
 
 NMEAGPS gps;
-gps_fix fix;
+gps_fix currentFix; // Stores the current GPS coordinates
 
 void setup() {
   Serial.begin(9600);
   gpsSerial.begin(GPSBaud);
-  
+
   Wire.begin(0x15); // I2C slave address
   Wire.onRequest(sendGPSData); // Set onRequest handler
 
@@ -23,35 +23,27 @@ void setup() {
 
 void loop() {
   if (gps.available(gpsSerial)) {
-    fix = gps.read();  // Read the GPS data
+    currentFix = gps.read(); // Read new GPS data
 
-    // Output latitude, longitude, and course if valid
-    if (fix.valid.location) {
       Serial.print(F("Location: "));
-      Serial.print(fix.latitude(), 6);
+      Serial.print(currentFix.latitude(), 6);
       Serial.print(F(", "));
-      Serial.print(fix.longitude(), 6);
-
-      if (fix.valid.heading) {
-        Serial.print(F(", Course Angle: "));
-        Serial.print(fix.heading());
-        Serial.println(F(" deg"));
-      } else {
-        Serial.println(F(", Course Angle: invalid"));
-      }
-    }
+      Serial.print(currentFix.longitude(), 6);
+      Serial.print(F(", Course: "));
+      Serial.print(currentFix.heading(), 2);
+      Serial.println(F(" deg"));
   }
 }
 
 // Function to send GPS data when requested
 void sendGPSData() {
-  if (fix.valid.location) {
-    float latitude = fix.latitude();
-    float longitude = fix.longitude();
-    int16_t course_scaled = (fix.valid.heading ? (int16_t)(fix.heading() * 100) : 0);  // Scale by 100 to keep precision, -1 if invalid
+  if (currentFix.valid.location) {
+    float latitude = currentFix.latitude();
+    float longitude = currentFix.longitude();
+    int16_t heading_scaled = (int16_t)(currentFix.heading() * 100); // Scale by 100 to keep precision
 
     Wire.write((byte*)&latitude, sizeof(latitude));  // Send latitude
     Wire.write((byte*)&longitude, sizeof(longitude));  // Send longitude
-    Wire.write((byte*)&course_scaled, sizeof(course_scaled));  // Send course
+    Wire.write((byte*)&heading_scaled, sizeof(heading_scaled));  // Send heading
   }
 }
